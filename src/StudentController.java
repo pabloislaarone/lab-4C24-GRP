2fd8445a4c8850b8f5d66efbef99f7fb983dd7a2
Pablo <pablo.isla@tecsup.edu.pe>
2026-03-24 11:58:43 -0500

    Update StudentController in sprint-1

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,8 +1,7 @@
 public class StudentController{
 
     public void controller(){
-        System.out.println("Controlando estudiantes");
+        System.out.println("Responsable trabajando en sprint-1");
     }
 
-
-}
+}}
